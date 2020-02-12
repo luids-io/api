bd7b9929bab61812f3f8fa36681b264d1312d764
Luis Guillen Civera <luisguillenc@gmail.com>
2020-02-12 14:37:04 +0100

    moved services from core to repository

---
+++ schemas/dnsutil/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# gets script dir
+SRCDIR=$(dirname $(readlink -f "$0"))
+
+# sets path for tools
+GOPATH="${GOPATH:-$HOME/go}"
+PATH=$GOPATH/bin:$PATH
+
+protoc -I $GOPATH/src -I . resolv.proto --go_out=plugins=grpc:$GOPATH/src
+protoc -I $GOPATH/src -I . archive.proto --go_out=plugins=grpc:$GOPATH/src
---
+++ schemas/tlsutil/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# gets script dir
+SRCDIR=$(dirname $(readlink -f "$0"))
+
+# sets path for tools
+GOPATH="${GOPATH:-$HOME/go}"
+PATH=$GOPATH/bin:$PATH
+
+protoc -I $GOPATH/src -I . common.proto --go_out=plugins=grpc:$GOPATH/src
+protoc -I $GOPATH/src -I . archive.proto --go_out=plugins=grpc:$GOPATH/src
+protoc -I $GOPATH/src -I . classify.proto --go_out=plugins=grpc:$GOPATH/src
