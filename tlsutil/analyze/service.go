1da9d96e77cc3bc1199b49b1895d2a913205dcec
Luis Guillen Civera <luisguillenc@gmail.com>
2020-04-08 09:39:04 +0200

    tlsutil: add analyzer service

---
+++ tlsutil/analyze/service.go
@@ -0,0 +1,53 @@
+// Copyright 2019 Luis Guillén Civera <luisguillenc@gmail.com>. See LICENSE.
+
+package analyze
+
+import (
+	"io"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/luids-io/api/protogen/tlsutilpb"
+	"github.com/luids-io/api/tlsutil/encoding"
+	"github.com/luids-io/core/tlsutil"
+)
+
+// Service implements a service wrapper for the grpc api
+type Service struct {
+	factory tlsutil.AnalyzerFactory
+}
+
+// NewService returns a new Service for the grpc api
+func NewService(f tlsutil.AnalyzerFactory) *Service {
+	return &Service{factory: f}
+}
+
+// RegisterServer registers a service in the grpc server
+func RegisterServer(server *grpc.Server, service *Service) {
+	pb.RegisterAnalyzeServer(server, service)
+}
+
+// SendMessages manage messages
+func (s *Service) SendMessages(stream pb.Analyze_SendMessagesServer) error {
+	analyzer := s.factory.NewAnalyzer()
+	defer analyzer.Close()
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+		msg := encoding.MessageRequest(req)
+		analyzer.SendMessage(msg)
+	}
+
+}
+
+//mapping errors
+func (s *Service) mapError(err error) error {
+	return status.Error(codes.Unavailable, err.Error())
+}
---
+++ tlsutil/encoding/message.go
@@ -0,0 +1,80 @@
+package encoding
+
+import (
+	"errors"
+	"net"
+
+	"github.com/golang/protobuf/ptypes"
+
+	pb "github.com/luids-io/api/protogen/tlsutilpb"
+	"github.com/luids-io/core/tlsutil"
+)
+
+// MessageRequestPB serialize to PB
+func MessageRequestPB(src *tlsutil.Msg) *pb.SendMessageRequest {
+	dst := &pb.SendMessageRequest{}
+	dst.Type = pb.MsgType(src.Type)
+	dst.StreamId = src.StreamID
+	if src.Type == tlsutil.OpenMsg && src.Open != nil {
+		dst.Open = &pb.MsgOpen{}
+		dst.Open.SourceIp = src.Open.SrcIP.String()
+		dst.Open.SourcePort = uint32(src.Open.SrcPort)
+		dst.Open.DestinationIp = src.Open.DstIP.String()
+		dst.Open.DestinationPort = uint32(src.Open.DstPort)
+		return dst
+	}
+	if src.Type == tlsutil.DataMsg && src.Data != nil {
+		dst.Data = &pb.MsgData{}
+		dst.Data.Timestamp, _ = ptypes.TimestampProto(src.Data.Timestamp)
+		dst.Data.Bytes = int32(src.Data.Bytes)
+		dst.Data.SawStart = src.Data.SawStart
+		dst.Data.SawEnd = src.Data.SawEnd
+		if len(src.Data.Records) > 0 {
+			dst.Data.Records = make([][]byte, 0, len(src.Data.Records))
+			for _, records := range src.Data.Records {
+				dst.Data.Records = append(dst.Data.Records, records)
+			}
+		}
+		if src.Data.Error != nil {
+			dst.Data.MsgErr = src.Data.Error.Error()
+		}
+		return dst
+	}
+	return dst
+}
+
+// MessageRequest unserialize  PB
+func MessageRequest(src *pb.SendMessageRequest) *tlsutil.Msg {
+	dst := &tlsutil.Msg{}
+	dst.Type = tlsutil.MsgType(src.GetType())
+	dst.StreamID = src.GetStreamId()
+	if dst.Type == tlsutil.OpenMsg && src.GetOpen() != nil {
+		openpb := src.GetOpen()
+		dst.Open = &tlsutil.MsgOpen{}
+		dst.Open.SrcIP = net.ParseIP(openpb.GetSourceIp())
+		dst.Open.DstIP = net.ParseIP(openpb.GetDestinationIp())
+		dst.Open.SrcPort = int(openpb.GetSourcePort())
+		dst.Open.DstPort = int(openpb.GetDestinationPort())
+		return dst
+	}
+	if dst.Type == tlsutil.DataMsg && src.GetData() != nil {
+		datapb := src.GetData()
+		dst.Data = &tlsutil.MsgData{}
+		dst.Data.Timestamp, _ = ptypes.Timestamp(datapb.GetTimestamp())
+		dst.Data.Bytes = int(datapb.GetBytes())
+		dst.Data.SawStart = datapb.GetSawStart()
+		dst.Data.SawEnd = datapb.GetSawEnd()
+		if len(datapb.GetRecords()) > 0 {
+			pbrecords := datapb.GetRecords()
+			dst.Data.Records = make([][]byte, 0, len(pbrecords))
+			for _, records := range pbrecords {
+				dst.Data.Records = append(dst.Data.Records, records)
+			}
+		}
+		if datapb.GetMsgErr() != "" {
+			dst.Data.Error = errors.New(datapb.GetMsgErr())
+		}
+		return dst
+	}
+	return dst
+}
